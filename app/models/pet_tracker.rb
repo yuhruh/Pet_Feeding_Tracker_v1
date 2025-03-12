e25c46c296e8bc3d0ec3fdad5b6f8ac5857e1468
Rita Huang <yuhruhgrape@gmail.com>
2025-03-13 00:43:23 +0800

    reset back to 8f43678

--- app/models/pet_tracker.rb
+++ app/models/pet_tracker.rb
@@ -1,2 +1,8 @@
 class PetTracker < ApplicationRecord
+  validates :brand, presence: true, length: {minimum: 2, maximum: 50}
+  validates :description, presence: true, length: {minimum: 6, maximum: 100}
+  validates :wet_amount, numericality: true
+  validates :wet_left_amount, numericality: true
+  validates :dry_amount, numericality: true
+  validates :dry_left_amount, numericality: true
 end
