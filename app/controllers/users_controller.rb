859dbe46c1d43292fe1345de6543dd9381157688
Rita Huang <yuhruhgrape@gmail.com>
2025-03-14 22:48:41 +0800

    add edit template and update action on users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
 
@@ -13,6 +17,19 @@ class UsersController < ApplicationController
         format.json { render :show, status: :created, location: @pet_tracker }
       else
         format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to pet_trackers_path, notice: "Account was successfully updated." }
+        format.json { render :show, status: :ok, location: @pet_tracker }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
