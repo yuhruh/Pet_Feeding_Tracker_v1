b6f99e69af1125b999377abb60c24d626f2dc17e
Rita Huang <yuhruhgrape@gmail.com>
2025-03-16 21:58:52 +0800

    change delete to get logout in routes.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,16 +7,16 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:notice] = "Logged in successfully."
-      redirect_to pet_trackers_path
+      redirect_to user
     else
-      flash.now[:alert] = "There was something wrong with your login details"
-      render 'new'
+      flash.now[:message] = "There was something wrong with your login details"
+      render action: 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Logged out"
+    flash[:notice] = "Logged out successfully!!"
     redirect_to root_path
   end
 end
