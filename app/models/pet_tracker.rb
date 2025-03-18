1891930fbfb7597cf4fd5c48f7afe9c88d957f1b
Rita Huang <yuhruhgrape@gmail.com>
2025-03-18 11:42:15 +0800

    modify validation of description length from 6 to 3

--- app/models/pet_tracker.rb
+++ app/models/pet_tracker.rb
@@ -1,7 +1,7 @@
 class PetTracker < ApplicationRecord
   belongs_to :user
   validates :brand, presence: true, length: {minimum: 2, maximum: 50}
-  validates :description, presence: true, length: {minimum: 6, maximum: 100}
+  validates :description, presence: true, length: {minimum: 3, maximum: 100}
   validates :wet_amount, numericality: true
   validates :wet_left_amount, numericality: true
   validates :dry_amount, numericality: true
