066ea571dc4490cb30fbde229a005ec295368532
Rita Huang <yuhruhgrape@gmail.com>
2025-03-18 21:52:34 +0800

    add input validation and pluralize

--- app/models/pet_tracker.rb
+++ app/models/pet_tracker.rb
@@ -2,8 +2,8 @@ class PetTracker < ApplicationRecord
   belongs_to :user
   validates :brand, presence: true, length: {minimum: 2, maximum: 50}
   validates :description, presence: true, length: {minimum: 3, maximum: 100}
-  validates :wet_amount, numericality: true
-  validates :wet_left_amount, numericality: true, comparison: { less_than: :wet_amount }
-  validates :dry_amount, numericality: true
-  validates :dry_left_amount, numericality: true, comparison: { less_than: :dry_amount }
+  validates :wet_amount, numericality: true, comparison: { greater_than: 0 }
+  validates :wet_left_amount, numericality: true, comparison: { less_than: :wet_amount, greater_than_or_equal_to: 0 }
+  validates :dry_amount, numericality: true, comparison: { greater_than: 0 }
+  validates :dry_left_amount, numericality: true, comparison: { less_than: :dry_amount, greater_than_or_equal_to: 0}
 end
